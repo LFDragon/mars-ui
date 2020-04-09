32e4c803cf7d7312c9f559209edc75672ba4aaf4
Jacky Long <jackylonggh@hotmail.com>
2020-04-09 23:11:02 +0800

    change banner

--- src/layout/BasicLayout.js
+++ src/layout/BasicLayout.js
@@ -4,12 +4,24 @@ import styles from './BasicLayout.less';
 
 const { Header, Footer, Sider, Content } = Layout;
 
-const img = {
-    marginTop: '10px',
-    width: '160px',
+const wrapper = {
+    display: 'flex',
     marginLeft: 'auto',
     marginRight: 'auto'
 };
+const img = {
+    marginTop: '8px',
+    marginBottom: '10px',
+    width: '160px',
+    height: '160px'
+};
+const bannerText = {
+    marginLeft: '20px',
+    fontSize: '26px',
+};
+const redColor = {
+    color: '#E51414'
+}
 
 class BasicLayout extends Component {
     render() {
@@ -19,13 +31,12 @@ class BasicLayout extends Component {
                     <Row type="flex" justify="center">
                         <Col span={18}>
                             <div style={{ padding: 24, minHeight: 360 }}>
-                                <span className={styles['override-ant-carousel']}>
-                                    <Carousel>
-                                        <div>
-                                            <img style={img} src={require('../assets/img/mars_logo.png')} alt={'Mars Logo'}></img>
-                                        </div>
-                                    </Carousel>
-                                </span>
+                                <div className={styles['my-banner']}>
+                                    <div style={wrapper}>
+                                        <img style={img} src={require('../assets/img/mars_logo.png')} alt={'Mars Logo'}></img>
+                                        <div style={bannerText}><span style={redColor}>M</span>eeting-room <span style={redColor}>A</span>utomation <span style={redColor}>R</span>evolution <span style={redColor}>S</span>ystem</div>
+                                    </div>
+                                </div>
                                 <div>
                                     {this.props.children}
                                 </div>
